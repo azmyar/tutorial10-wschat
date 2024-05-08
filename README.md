7e0c292c34ca47138075b7767585efc3dafa7411
azmyar <azmy.arya.rizaldi@gmail.com>
2024-05-08 18:16:30 +0700

    Experiment 3.2: Be Creative

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 
 ## Original Code of Yew 'Websocket' Chat
 <img src="img/chat-1.png">
+
+## Be Creative
+<img src="img/chat-2.png">
+<img src="img/chat-3.png">
+Changed the login page with an illustration form pinterest, and also changed the color palette.
--- src/components/login.rs
+++ src/components/login.rs
@@ -27,12 +27,20 @@ pub fn login() -> Html {
     };
     
     html! {
-        <div class="bg-gray-800 flex w-screen">
-            <div class="container mx-auto flex flex-col justify-center items-center	">
-                <form class="m-4 flex">
-                    <input {oninput} class="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" placeholder="Username"/>
-                    <Link<Route> to={Route::Chat}> <button {onclick} disabled={username.len()<1} class="px-8 rounded-r-lg bg-violet-600	  text-white font-bold p-4 uppercase border-violet-600 border-t border-b border-r" >{"Go Chatting!"}</button></Link<Route>>
-                </form>
+        <div class="bg-[#F7F7F7] flex w-screen justify-center items-center">
+            <div class="flex flex-row justify-center h-fit">
+                <img src="https://i.pinimg.com/564x/25/b0/61/25b0615c881479c9ff19a19e918203f6.jpg" class= "w-[470px] h-[470px]"/>
+                <div class="container mx-auto flex flex-col justify-center items-left	 h-fill">
+
+                    <div class="text-[#7C4097] text-6xl font-bold p-5" >
+                        <p> {"YewChat"}</p>
+                    </div>
+
+                    <form class="m-4 flex">
+                        <input {oninput} class="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" placeholder="Username"/>
+                        <Link<Route> to={Route::Chat}> <button {onclick} disabled={username.len()<1} class="px-8 rounded-r-lg bg-[#7C4097] text-white font-bold p-4 uppercase border-[#7C4097] border-t border-b border-r" >{"Go Chatting!"}</button></Link<Route>>
+                    </form>
+                </div>
             </div>
         </div>
     }
